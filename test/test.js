1be2019a425c392c165009e374b30b9e98abd76e
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-22 05:34:55 -0700

    TDD Cycle 2

--- test/test.js
+++ test/test.js
@@ -1,15 +1,17 @@
 const { assert } = require('chai');
 
 describe('multiply test', () => {
-  it('first test', () => {
+  it('Test First TDD Cycle 1', () => {
     // use assert here
     function multiply(a, b) { return 1; }
     assert.equal(multiply(1, 1), 1);
   });
-  it('second test', () => {
+  it('Test First TDD Cycle 2', () => {
     // use assert here
+    function multiply(a, b) { return 4; }
+    assert.equal(multiply(2, 2),4)
   });
-  it('multiply test', () => {
+  it('Test First TDD Cycle 3', () => {
     // use assert here
 
   });
