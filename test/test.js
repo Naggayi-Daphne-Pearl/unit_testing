15c4fbe9d04f44d645171fecd537cacc12bdd4c7
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-22 05:24:21 -0700

    Test First TDD Cycle 1

--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const { assert } = require('chai');
+
+describe('multiply test', () => {
+  it('first test', () => {
+    // use assert here
+    function multiply(a, b) { return 1; }
+    assert.equal(multiply(1, 1), 1);
+  });
+  it('second test', () => {
+    // use assert here
+  });
+  it('multiply test', () => {
+    // use assert here
+
   });
 });
