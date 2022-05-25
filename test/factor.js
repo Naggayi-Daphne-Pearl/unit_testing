25ebc5b59d42063eb4a7aed7d0d0e885b22b450e
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-25 01:12:44 -0700

    Testing using Factorial

--- test/factor.js
+++ test/factor.js
@@ -1,4 +1,5 @@
 function factorialize(num) {
+    
 // validating our input. 
   // If the number is less than 0, reject it.
   if (num < 0) 
@@ -15,6 +16,9 @@ function factorialize(num) {
 }   
 
 module.exports = factorialize;
+
+// Pair programmer 
+// Alwoch Sophia
 
 // references
 // https://www.freecodecamp.org/news/how-to-factorialize-a-number-in-javascript-9263c89a4b38/#:~:text=function%20factorialize(num)%20%7B%20%2F%2F%20If%20num%20%3D%200%20OR,num%20*%3D%20i%3B%20%2F*%20num
